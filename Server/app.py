52dcb43ba01482a7f90e5245a04a70f034ed497e
Pratik Nalage <pratiknalage@Ps-MacBook.local>
2022-05-06 21:06:33 -0400

    Final release

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,8 @@
 from distutils.log import debug
 import sys
 import sqlite3
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
+import intent_management as it
 
 app = Flask(__name__)
 
@@ -21,7 +22,8 @@ def webhook():
 
     # Create a connection to DB and dump the query
     con, cur = connect_db()
-    execute_queries_db("INSERT INTO qna VALUES ('" + dict(req)['queryResult']['queryText'] + "','')", cur, True, con)
+    # execute_queries_db("INSERT INTO qna VALUES ('abc', '', 0)", cur, True, con)
+    execute_queries_db("INSERT INTO qna (question, answer, status) VALUES ('" + dict(req)['queryResult']['queryText'] + "', '', 0)", cur, True, con)
     con.close()
 
     # Return sample response to the user
@@ -30,16 +32,37 @@ def webhook():
     return res
 
 
-@app.route('/qna')
+@app.route('/qna', methods = ['POST', 'GET'])
 def qna():
-    pass
+    con, cur = connect_db()
+    if request.method == 'POST':
+        id = request.form['id']
+        question = request.form['question']
+        answer = str(request.form['answer']).replace("'", "")
+        print(answer,question, flush=True)
+        execute_queries_db("UPDATE qna SET answer='"+str(answer)+"', status = 1 WHERE id = "+str(id), cur, True, con)
+        it.create_intent("remoteassistantbot-xjmv", id, [question], [answer])
+        # print(request.json, flush=True)
+        # print(request.form['id'], flush=True)
+        # print(request.form['question'], flush=True)
+
+
+    questions = {}
+    for row in execute_queries_db("SELECT * FROM qna where status = 0", cur):
+        questions[row[0]] = row[1]
+    # return str(questions)
+    # print(questions)
+    return render_template('index.html', questions=questions)
 
 
 def execute_queries_db(query, cur, commit=False, con=""):
     # Execute Queries - Insert, Update, Delete
-    cur.execute(query)
+    print(query, flush=True)
+    query_output = cur.execute(query)
     if commit:
         con.commit()
+    print(query_output, flush=True)
+    return query_output
 
 
 def connect_db():
@@ -54,8 +77,10 @@ def create_database():
     # Create Database and tables
     con, cur = connect_db()
     execute_queries_db("DROP TABLE qna", cur, True, con)
-    execute_queries_db("CREATE TABLE qna (questions text, answers text)", cur, True, con)
-    execute_queries_db("INSERT INTO qna VALUES ('sample-question','sample-answer')", cur, True, con)
+    execute_queries_db("CREATE TABLE qna (id integer primary key AUTOINCREMENT, question text, answer text, status boolean)", cur, True, con)
+    execute_queries_db("INSERT INTO qna (question, answer, status) VALUES ('sample-question','sample-answer', 1)", cur, True, con)
+    execute_queries_db("INSERT INTO qna (question, answer, status) VALUES ('sample-question','sample-answer', 1)", cur, True, con)
+
     con.close()
     return "Database created sucessfully"
 
